e907371cb0133d88bfba9b98f00f9b99e1d95d5b
viniciusMonteiroOrlandi <dev.viniciusmonteiro@gmail.com>
2025-09-11 10:19:55 -0300

    Remove unnecessary files and clean build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  experimental: {
+    outputFileTracingIgnores: ['backup-*.json', '*.js']
+  }
 }
 
 module.exports = nextConfig
