cdc8530610266ea7a5107c3d197913590082a416
viniciusMonteiroOrlandi <dev.viniciusmonteiro@gmail.com>
2025-09-11 10:48:08 -0300

    Restore automatic redirect to login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
+import { GetServerSideProps } from 'next';
+
 export default function Index() {
-  return (
-    <div>
-      <h1>Sistema de Quilometragem</h1>
-      <p>Página inicial funcionando!</p>
-      <a href="/login">Ir para Login</a>
-    </div>
-  );
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    redirect: {
+      destination: '/login',
+      permanent: false,
+    },
+  };
+};
--- pages/test.tsx
+++
@@ -1,8 +0,0 @@
-export default function Test() {
-  return (
-    <div>
-      <h1>Página de Teste</h1>
-      <p>Se você está vendo isso, o Vercel está funcionando!</p>
-    </div>
-  );
-}
